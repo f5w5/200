b5424980385bec3fc2285941426e7ed7a541c8c5
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-08-23 01:01:07 +0300

    Update SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -1,6 +1,6 @@
 package;
 
-#if (android && MODS_ALLOWED)
+#if android
 import android.AndroidTools;
 import android.stuff.Permissions;
 #end
@@ -22,14 +22,14 @@ import flash.system.System;
 using StringTools;
 
 class SUtil {
-	#if (android && MODS_ALLOWED)
-	private static var grantedPermsList:Array<Permissions> = AndroidTools.getGrantedPermissions(); 
+	#if android
+	private static var grantedPermsList:Array<Permissions> = AndroidTools.getGrantedPermissions(); // granted Permissions
 	private static var aDir:String = null; // android dir 
-	public static var sPath:String = AndroidTools.getExternalStorageDirectory(); // storage dir
+	private static var sPath:String = AndroidTools.getExternalStorageDirectory(); // storage dir
 	#end
 
 	static public function getPath():String {
-		#if (android && MODS_ALLOWED)
+		#if android
 		if (aDir != null && aDir.length > 0) {
 			return aDir;
 		} else {
@@ -42,7 +42,7 @@ class SUtil {
 	}
 
 	static public function doTheCheck() {
-		#if (android && MODS_ALLOWED)
+		#if android
 		if (!grantedPermsList.contains(Permissions.READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
 			if (AndroidTools.sdkVersion > 23 || AndroidTools.sdkVersion == 23) {
 				AndroidTools.requestPermissions([Permissions.READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE]);
@@ -68,10 +68,9 @@ class SUtil {
 		if (!FileSystem.exists(SUtil.getPath() + "saves")){
 			FileSystem.createDirectory(SUtil.getPath() + "saves");
 		}
-		if (!FileSystem.exists(SUtil.getPath() + "mods"){
+		if (!FileSystem.exists(SUtil.getPath() + "mods")){
 			File.saveContent(SUtil.getPath() + "Paste the Assets and Mods folders here.txt", "the file name says all");
 		}
-
 		if (!FileSystem.exists(SUtil.getPath() + "mods")){
 			SUtil.applicationAlert("Instructions:", "You have to copy assets/mods from apk to your internal storage app directory" 
 				+ " ( here " + SUtil.getPath() + " )" 
@@ -79,7 +78,7 @@ class SUtil {
 				+ "\n" + "Press Ok To Close The App");
 			System.exit(0);
 		}
-		if (FileSystem.exists(SUtil.getPath() + "Paste the Assets and Mods folders here.txt") && FileSystem.exists(SUtil.getPath() + "mods"){
+		if (FileSystem.exists(SUtil.getPath() + "Paste the Assets and Mods folders here.txt") && FileSystem.exists(SUtil.getPath() + "mods")){
 			FileSystem.deleteFile(SUtil.getPath() + "Paste the Assets and Mods folders here.txt");
 		}
 		#end
@@ -108,13 +107,11 @@ class SUtil {
 
 		errMsg += e.error;
 
-		#if MODS_ALLOWED
 		if (!FileSystem.exists(SUtil.getPath() + "crash")){
 			FileSystem.createDirectory(SUtil.getPath() + "crash");
 		}
 
 		File.saveContent(SUtil.getPath() + path, errMsg + "\n");
-		#end
 
 		Sys.println(errMsg);
 		Sys.println("Crash dump saved in " + Path.normalize(path));
@@ -130,17 +127,17 @@ class SUtil {
 
 	#if android
 	static public function saveContent(fileName:String = "file", fileExtension:String = ".json", fileData:String = "you forgot something to add in your code"){
-		#if (android && MODS_ALLOWED)
 		if (!FileSystem.exists(SUtil.getPath() + "saves")){
 			FileSystem.createDirectory(SUtil.getPath() + "saves");
 		}
 
 		File.saveContent(SUtil.getPath() + "saves/" + fileName + fileExtension, fileData);
 		SUtil.applicationAlert("Done Action :)", "File Saved Successfully!");
-		#elseif android
+	}
+
+	static public function saveClipboard(fileName:String = "file", fileExtension:String = ".json", fileData:String = "you forgot something to add in your code"){
 		openfl.system.System.setClipboard(fileData);
 		SUtil.applicationAlert("Done Action :)", "Data Saved to Clipboard Successfully!");
-		#end
 	}
 
 	static public function copyContent(copyPath:String, savePath:String) {
@@ -149,5 +146,5 @@ class SUtil {
 			File.saveBytes(savePath, bytes);
 		}
 	}
-        #end
+	#end
 }
