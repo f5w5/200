b2f1dab33722a36ca7bf5c4dc390d67dddface45
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2022-08-23 22:37:57 +0300

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -347,7 +347,6 @@ class Paths
 	// completely rewritten asset loading? fuck!
 	public static var currentTrackedAssets:Map<String, FlxGraphic> = [];
 	public static var currentTrackedTextures:Map<String, Texture> = [];
-	public static var currentTrackedSounds:Map<String, Sound> = [];
 
 	public static function returnGraphic(key:String, ?library:String, ?textureCompression:Bool = false)
 	{
